cecfada59aeb440106a3b3e2d93da15e3d5363c4
reya-prm <reyapermana122@gmail.com>
2026-05-10 20:57:24 +0700

    bikin modal (layout edit)

--- Js/script.js
+++ Js/script.js
@@ -2,3 +2,13 @@ function togglePassword() {
     const input = document.getElementById('password');
     input.type = input.type === 'password' ? 'text' : 'password';
 }
+
+function bukaModal(id, judul) {
+    document.getElementById('editId').value = id; // pakai parameter id
+    document.getElementById('editJudul').value = judul; // pakai parameter judul
+    document.getElementById('modalEdit').style.display = 'flex'; // buka edit
+}
+
+function tutupModal() {
+    document.getElementById('modalEdit').style.display = 'none'; // tutup edit
+}
--- update_list.php
+++ update_list.php
@@ -4,13 +4,12 @@ include 'koneksi.php';
 
 $user_id = $_SESSION['user_id'];
 
-// Tambah todo
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['judul'])) {
-    $judul = $_POST['judul'];
-    $stmt = mysqli_prepare($conn, "INSERT INTO todos (user_id, judul) VALUES (?, ?)");
-    mysqli_stmt_bind_param($stmt, "is", $user_id, $judul);
-    mysqli_stmt_execute($stmt);
-    header("Location: update_list.php");
+// Edit todo - Tambah Todo
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $judul = $_POST['editjudul'];
+    mysqli_query($conn, "UPDATE todos SET judul='$judul' WHERE id=$id AND user_id=$user_id");
+    header("Location: update_list.php?success=edit");
     exit();
 }
 
@@ -43,7 +42,11 @@ $todos = mysqli_query($conn, "SELECT * FROM todos WHERE user_id = $user_id ORDER
             <input type="text" name="judul" placeholder="Contoh: Belajar PHP" required>
             <button type="submit">Tambah Aktivitas</button>
         </form>
-        
+    
+    <?php if(isset($_GET['success']) && $_GET['success'] == 'edit'): ?>
+    <p style="color:green">Aktivitas berhasil diubah!</p>
+    <?php endif; ?>   
+
     <?php if(mysqli_num_rows($todos) > 0): ?>
     <table>
         <tr>
@@ -56,7 +59,7 @@ $todos = mysqli_query($conn, "SELECT * FROM todos WHERE user_id = $user_id ORDER
             <td><?php echo $todo['judul']; ?></td>
             <td><?php echo $todo['status'] == 1 ? '<span style="color:#5aad7e; font-weight:700;">Selesai</span>' : '<span style="color:#e05c5c; font-weight:700;">Belum</span>' ?></td>
             <td>
-                <a href="edit_todo.php?id=<?php echo $todo['id']; ?>">Edit</a>
+                <a href="#" onclick="bukaModal(<?php echo $todo['id']; ?>, '<?php echo $todo['judul']; ?>')">Edit</a>
                 <a href="?hapus=<?php echo $todo['id']; ?>">Hapus</a>
             </td>
         </tr>
@@ -64,5 +67,16 @@ $todos = mysqli_query($conn, "SELECT * FROM todos WHERE user_id = $user_id ORDER
     </table>
 <?php endif; ?>
     </div>
+    <div id="modalEdit" style="display:none">
+        <form method="POST">
+            <input type="hidden" name="id" id="editId">
+            <label>Edit Aktivitas</label>
+            <input type="text" name="editjudul" id="editJudul" placeholder="Contoh: Menulis" required>
+            <button type="submit">Edit Aktivitas</button>
+            <button type="button" onclick="tutupModal()">Batal</button>
+        </form>
+    </div>
+
+    <script src="Js/script.js"></script>   
 </body>
 </html>
